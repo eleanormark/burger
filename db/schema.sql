5f0b5e6863c1a1f341a78e4a1909f4a7466f03b4
Eleanor Mark <emark@calvisitor-10-105-159-141.calvisitor.1918.berkeley.edu>
2017-05-06 10:03:21 -0700

    added framework structure

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers (
+   id INT( 11 ) AUTO_INCREMENT NOT NULL,
+   burger_name VARCHAR( 255) NOT NULL,
+   devoured BOOLEAN NOT NULL DEFAULT false,
+   created_at TIMESTAMP NOT NULL,
+   PRIMARY KEY ( id )
+);
---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+--run in command line: mysql.server start
+--Start MySQL command line tool and login: mysql -u root -p.
+--Without password: mysql -u root
+--With the mysql> command line tool running, enter the command source schema.sql. This will run your schema file and all of the queries in it -- in other words, you'll be creating your database.
+--Now insert the entries you defined in seeds.sql by running the file: source seeds.sql.
+--Close out of the MySQL command line tool: exit.
+
+create database if not exists burger_db;
+
+use burger_db;
+
+create table if not exists burgers (
+   id INT( 11 ) AUTO_INCREMENT NOT NULL,
+   burger_name VARCHAR( 255) NOT NULL,
+   devoured BOOLEAN NOT NULL DEFAULT false,
+   created_at TIMESTAMP NOT NULL,
+   PRIMARY KEY ( id )
+);
+
+insert into burgers (burger_name, devoured)
+values
+('sweet potato burger', false),
+('portobello mushroom burger', false),
+('black bean burger', false);
+
+--select * from burgers;
